5867c679da63f8f6417a66e6a6efa59168255d29
pavliv182 <uptn182@gmail.com>
2022-12-14 18:28:49 +0200

    mongoose

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    });
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 // const app = require('./app')
 
 // app.listen(3000, () => {
