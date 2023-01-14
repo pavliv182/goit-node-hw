c1636f2b73834eac61f8f656261ed188208dd052
pavliv182 <uptn182@gmail.com>
2023-01-14 15:37:22 +0200

    owner, addContact, listContacts

--- models/user.js
+++ models/user.js
@@ -30,11 +30,6 @@ const userSchema = new Schema(
       type: String,
       default: null,
     },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-      required: true,
-    },
   },
   { versionKey: false, timestamps: true }
 );
