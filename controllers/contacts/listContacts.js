a044d12d260bbc2c8274856b8826d68765c0036c
pavliv182 <uptn182@gmail.com>
2023-01-14 15:56:05 +0200

    listContacts .populate

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,7 +2,13 @@ const { Contact } = require("../../models/contact");
 
 const listContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  // const filter = typeof favorite === "boolean" ? { favorite } : {};
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
   res.status(200).json(result);
 };
 
